d87c1c9a292394e13ac1efa14d246c331d975b88
Graeme Browning <graeme.h.browning@gmail.com>
2015-06-22 10:42:51 -0400

    Finished following chat application tutorial

---
+++ gameclient.js
@@ -0,0 +1,86 @@
+window.onload = function() {
+    //do stuff here if you want
+    var content = document.getElementById("content");
+    var status = document.getElementById("status");
+    var input = document.getElementById("input");
+
+    var myColor = false;
+    var myName = false;
+
+
+    if (!('WebSocket' in window)) {
+        console.log('websockets not supported');
+        return;
+    }
+
+    //websocket is supported!
+    var connection = new WebSocket('ws://52.24.149.122:1337');
+    connection.onopen = function() {
+        console.log('connection open!');
+        input.disabled = false;
+        status.textContent = 'Choose name:';
+    };
+    connection.onclose = function() {
+        console.log('connection closed');
+    };
+    connection.onerror = function(error) {
+        console.log('Error detected: ' + error);
+        var errorNode = document.createElement('p');
+        var t = document.createTextNode('error connecting to server');
+        errorNode.appendChild(t);
+        content.appendChild(errorNode);
+    };
+    connection.onmessage = function(message) {
+        //try to parse JSON message
+        var json;
+        try {
+            json = JSON.parse(message.data);
+        } catch (e) {
+            console.log("invalid JSON: ", message.data);
+            return;
+        }
+
+        if (json.type === 'color') {
+            myColor = json.data;
+            //setup status
+            status.textContent = myName;
+            //TODO change colour
+            input.disabled = false;
+        } else if (json.type === 'history') {
+            for (var i = 0; i < json.data.length; i++) {
+                addMessage(json.data[i]);
+            }
+        } else if (json.type === 'message') {
+            addMessage(json.data);
+            input.disabled = false;
+        }
+    };
+
+    input.onkeypress = function(event) {
+        if (event.keyCode === 13) {
+            //enter key
+            var msg = this.value;
+            if (!msg) {
+                return;
+            }
+
+            connection.send(msg);
+
+            if(myName === false) {
+                myName = msg;
+            }
+
+            this.value = '';
+            input.disabled = true;
+        }
+    };
+
+    function addMessage(msgData) {
+        var messageNode = document.createElement('p');
+        // var t = document.createTextNode(msgData.time + '\t' + msgData.author + ': ' + msgData.text);
+        var t = document.createTextNode(msgData.author + ': ' + msgData.text);
+        messageNode.appendChild(t);
+        //content.insertBefore(messageNode, content.firstChild);
+        content.appendChild(messageNode);
+    }
+};
--- gameserver.js
+++ gameserver.js
@@ -1,30 +1,103 @@
+//set the process name (for use with commands like 'ps')
+process.title = 'gameserver';
+
+//modules
 var WebSocketServer = require('websocket').server;
 var http = require('http');
 
-var server = http.createServer(function (request, response) {
-    //http stuff, nothing
-});
-server.listen(1337, function() { });
+//optionally run server with a port specified on the command line
+var webSocketsServerPort = 1337;
+if (process.argv[2])
+    webSocketsServerPort = process.argv[2];
 
+//chat example variables
+//hitory of messages
+var history = [];
+//clients
+var clients = [];
+//text colours
+var colors = ['red', 'green', 'blue', 'magenta', 'purple', 'plum', 'orange'];
+//randomize colors
+colors.sort(function(a, b) {
+    return Math.random() > 0.5;
+});
+
+//http server
+var server = http.createServer(function(request, response) {});
+server.listen(webSocketsServerPort, function() {});
+
+//WebSocket server
 wsServer = new WebSocketServer({
     httpServer: server
 });
 
 wsServer.on('request', function(request) {
-    console.log('request received');
+    console.log((new Date()) + ' connection from ' + request.origin);
+
     var connection = request.accept(null, request.origin);
+    clients.push(connection);
+    var userName = false;
+    var userColor = false;
+
+    console.log((new Date()) + ' connection accepted');
+
+    //send chat history
+    if (history.length > 0) {
+        connection.sendUTF(JSON.stringify({
+            type: 'history',
+            data: history
+        }));
+    }
 
     connection.on('message', function(message) {
-        if(message.type === 'utf8') {
+        if (message.type === 'utf8') {
             //process WebSocket message
-            console.log('message received');
+            if (userName === false) {
+                //client is sending username
+                userName = message.utf8Data;
+
+                userColor = colors.shift();
+                connection.sendUTF(JSON.stringify({
+                    type: 'color',
+                    data: userColor
+                }));
+
+                console.log('userName: ' + userName + ' color: ' + userColor);
+            } else {
+                //client sending message
+                console.log((new Date()) + ' ' + userName + " : " + message.utf8Data);
+
+                var messageObj = {
+                    time: (new Date()).getTime(),
+                    text: message.utf8Data,
+                    author: userName,
+                    color: userColor
+                };
+                history.push(messageObj);
+                history = history.slice(-100);
+
+                //send message to clients
+                var json = JSON.stringify({
+                    type: 'message',
+                    data: messageObj
+                });
+                for (var i = 0; i < clients.length; i++) {
+                    clients[i].sendUTF(json);
+                }
+            }
         }
     });
 
-    connection.on('close', function (connection) {
-        //close user connection
-        console.log('connection closed');
+    connection.on('close', function(con) {
+        //user connection closed
+        if (userName !== false && userColor !== false) {
+            console.log((new Date()) + " Client " + connection.remoteAddress + " disconnected");
+            //remove client from array
+            clients.splice(clients.indexOf(connection), 1);
+            // return user color to pool
+            colors.push(userColor);
+        }
     });
 });
 
-console.log('started app');
+console.log((new Date()) + ' game server started');
