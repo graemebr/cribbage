a93a91b400b8e1212f9a9d45668a72a17dae586f
Graeme Browning <graeme.h.browning@gmail.com>
2015-07-11 18:45:36 -0400

    trying out one possible structure with singleton design pattern

--- gameclient.js
+++ gameclient.js
@@ -1,37 +1,189 @@
-window.onload = function() {
-    //do stuff here if you want
+var GameClient = (function() {
+    var instance;
 
-    var chatWindow = document.getElementById("chatWindow");
+    function init() {
+        var self = this;
+        //properties
+        this.chatWindow = document.getElementById("chatWindow");
+        this.messageManager = new MessageManager();
 
-    // var myColor = false;
-    var myName = false;
+
+        this.openLoginScreen = function() {
+            //hide connecting screen
+            $('#connecting').hide();
+            //show login screen
+            $('#chooseNamePanel').show();
+        };
+
+        this.openErrorScreen = function() {
+            $('#connecting').text("error connecting to server");
+        };
+
+        this.handleMessage = function(msg) {
+            switch(msg.type) {
+                case 'chatHistory':
+                    for (var i = 0; i < msg.data.length; i++) {
+                        self.addChatMessage(msg.data[i]);
+                    }
+                    break;
+                case 'chatMessage':
+                    self.addChatMessage(msg.data);
+                    $('#chatPanel input')[0].disabled = false;
+                    break;
+                case 'playerJoin':
+                    var playerName = document.createElement('li');
+                    playerName.appendChild(document.createTextNode(msg.data.name));
+                    playerName.style.display = 'none';
+                    playerName.setAttribute('id', msg.data.id);
+                    console.log(msg.data.id);
+                    $('#playersPanel ul').append(playerName);
+                    $('#' + msg.data.id).fadeIn('slow');
+                    self.addMessage(msg.data.name + ' connected', 'notice');
+                    break;
+                case 'playerLeave':
+                    $('#' + msg.data.id).fadeOut('slow');
+                    self.addMessage(msg.data.name + ' disconnected', 'error');
+                    break;
+            }
+        };
+
+        $('#chooseNamePanel input').keydown(function(event) {
+            if (event.keyCode === 13) {
+                //enter key
+                var msg = this.value;
+                if (!msg) {
+                    return false;
+                }
+
+                selfS.messageManager.send({
+                    type: 'name',
+                    data: msg
+                });
+
+                this.value = '';
+
+                //hide login/name choosing screen
+                $('#chooseNamePanel').hide();
+                //show main game screens
+                $('#gameCanvas').show();
+                $('#sidePanel').show();
+            }
+        });
+
+        $('#chooseNamePanel button').click(function(event) {
+            console.log("Clicked!!");
+            var msg = $('#chooseNamePanel input')[0].value;
+            if (!msg) {
+                return false;
+            }
+
+            self.messageManager.send({
+                type: 'name',
+                data: msg
+            });
+
+            $('#chooseNamePanel input')[0].value = '';
+
+            //hide login/name choosing screen
+            $('#chooseNamePanel').hide();
+            //show main game screens
+            $('#gameCanvas').show();
+            $('#sidePanel').show();
+        });
+
+        $('#chatPanel input').keydown(function(event) {
+            if (event.keyCode === 13) {
+                //enter key
+                var msg = this.value;
+                if (!msg) {
+                    return false;
+                }
+
+                self.messageManager.send({
+                    type: 'chat',
+                    data: msg
+                });
+
+                this.value = '';
+                $('#chatPanel input')[0].disabled = true;
+            }
+        });
+
+        $('#chatPanel button').click(function(event) {
+            var msg = $('#chatPanel input')[0].value;
+            if (!msg) {
+                return false;
+            }
+
+            self.messageManager.send({
+                type: 'chat',
+                data: msg
+            });
+
+            $('#chatPanel input')[0].value = '';
+            $('#chatPanel input')[0].focus();
+            $('#chatPanel input')[0].disabled = true;
+        });
+
+        this.addChatMessage = function(msgData) {
+            var nameNode = document.createElement('span');
+            nameNode.setAttribute('class', 'name');
+            nameNode.appendChild(document.createTextNode(msgData.author + ': '));
+            var messageNode = document.createElement('p');
+            messageNode.appendChild(nameNode);
+            messageNode.appendChild(document.createTextNode(msgData.text));
+            this.chatWindow.appendChild(messageNode);
+
+            this.chatWindow.scrollTop = chatWindow.scrollHeight;
+        };
+
+        this.addMessage = function(msg, cssClass) {
+            var messageNode = document.createElement('p');
+            messageNode.appendChild(document.createTextNode(msg));
+            messageNode.setAttribute('class', cssClass);
+            this.chatWindow.appendChild(messageNode);
+
+            this.chatWindow.scrollTop = chatWindow.scrollHeight;
+        };
+    }
+
+    return {
+        getInstance: function() {
+            if(!instance) {
+                instance = new init();
+            }
+
+            return instance;
+        }
+    };
+
+})();
 
 
+
+var MessageManager = function() {
     if (!('WebSocket' in window)) {
         console.log('websockets not supported');
         return;
     }
 
-    //websocket is supported!
-    var connection = new WebSocket('ws://52.24.149.122:1337');
-    connection.onopen = function() {
+    this.connection = new WebSocket('ws://52.24.149.122:1337');
+
+    this.connection.onopen = function() {
         console.log('connection open!');
-        //hide connecting screen
-        $('#connecting').hide();
-        //show login/name choosing screen
-        $('#chooseNamePanel').show();
+        GameClient.getInstance().openLoginScreen();
     };
-    connection.onclose = function() {
+
+    this.connection.onclose = function() {
         console.log('connection closed');
     };
-    connection.onerror = function(error) {
-        $('#connecting').text("error connectin to server");
-        // //hide connecting screen
-        // $('#connecting').hide();
-        // //show login/name choosing screen
-        // $('#chooseNamePanel').show();
+
+    this.connection.onerror = function(error) {
+        console.log('connection error!');
+        GameClient.getInstance().openErrorScreen();
     };
-    connection.onmessage = function(message) {
+
+    this.connection.onmessage = function(message) {
         //try to parse JSON message
         var json;
         try {
@@ -41,131 +193,16 @@ window.onload = function() {
             return;
         }
 
-        if (json.type === 'chatHistory') {
-            for (var i = 0; i < json.data.length; i++) {
-                addChatMessage(json.data[i]);
-            }
-        } else if (json.type === 'chatMessage') {
-            addChatMessage(json.data);
-            $('#chatPanel input')[0].disabled = false;
-        } else if (json.type === 'playerJoin') {
-            var playerName = document.createElement('li');
-            playerName.appendChild(document.createTextNode(json.data.name));
-            playerName.style.display = 'none';
-            playerName.setAttribute('id', json.data.id);
-            console.log(json.data.id);
-            $('#playersPanel ul').append(playerName);
-            $('#' + json.data.id).fadeIn('slow');
-            addMessage(json.data.name + ' connected', 'notice');
-        } else if (json.type === 'playerLeave') {
-            $('#'+json.data.id).fadeOut('slow');
-            addMessage(json.data.name + ' disconnected', 'error');
-        }
+        GameClient.getInstance().handleMessage(json);
     };
 
-    $('#chooseNamePanel input').keydown(function(event) {
-        if (event.keyCode === 13) {
-            //enter key
+    this.send = function(obj) {
+        this.connection.send(JSON.stringify(obj));
+    };
+};
 
-            var msg = this.value;
-            if (!msg) {
-                return false;
-            }
 
-            myName = msg;
-            var jsonMessage = JSON.stringify({
-                type: 'name',
-                data: msg
-            });
-            connection.send(jsonMessage);
 
-            this.value = '';
-
-            //hide login/name choosing screen
-            $('#chooseNamePanel').hide();
-            //show main game screens
-            $('#gameCanvas').show();
-            $('#sidePanel').show();
-        }
-    });
-
-    $('#chooseNamePanel button').click(function(event) {
-        console.log("Clicked!!");
-        var msg = $('#chooseNamePanel input')[0].value;
-        if (!msg) {
-            return false;
-        }
-
-        myName = msg;
-        var jsonMessage = JSON.stringify({
-            type: 'name',
-            data: msg
-        });
-        connection.send(jsonMessage);
-
-        $('#chooseNamePanel input')[0].value = '';
-
-        //hide login/name choosing screen
-        $('#chooseNamePanel').hide();
-        //show main game screens
-        $('#gameCanvas').show();
-        $('#sidePanel').show();
-    });
-
-    $('#chatPanel input').keydown(function(event) {
-        if (event.keyCode === 13) {
-            //enter key
-            var msg = this.value;
-            if (!msg) {
-                return false;
-            }
-
-            var jsonMessage = JSON.stringify({
-                type: 'chat',
-                data: msg
-            });
-            connection.send(jsonMessage);
-
-            this.value = '';
-            $('#chatPanel input')[0].disabled = true;
-        }
-    });
-
-    $('#chatPanel button').click(function(event) {
-        var msg = $('#chatPanel input')[0].value;
-        if (!msg) {
-            return false;
-        }
-
-        var jsonMessage = JSON.stringify({
-            type: 'chat',
-            data: msg
-        });
-        connection.send(jsonMessage);
-
-        $('#chatPanel input')[0].value = '';
-        $('#chatPanel input')[0].focus();
-        $('#chatPanel input')[0].disabled = true;
-    });
-
-    function addChatMessage(msgData) {
-        var nameNode = document.createElement('span');
-        nameNode.setAttribute('class', 'name');
-        nameNode.appendChild(document.createTextNode(msgData.author + ': '));
-        var messageNode = document.createElement('p');
-        messageNode.appendChild(nameNode);
-        messageNode.appendChild(document.createTextNode(msgData.text));
-        chatWindow.appendChild(messageNode);
-
-        chatWindow.scrollTop = chatWindow.scrollHeight;
-    }
-
-    function addMessage(msg, cssClass) {
-        var messageNode = document.createElement('p');
-        messageNode.appendChild(document.createTextNode(msg));
-        messageNode.setAttribute('class', cssClass);
-        chatWindow.appendChild(messageNode);
-
-        chatWindow.scrollTop = chatWindow.scrollHeight;
-    }
+window.onload = function() {
+    game = GameClient.getInstance();
 };
